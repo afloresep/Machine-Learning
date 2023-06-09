8bf27b50e6757a3a45c5bf7a6029b3057a0df1f6
alex desktop <afloresep01@gmail.com>
2023-06-09 17:50:15 +0200

    deleted

--- KNN_movies.py
+++ KNN_movies.py
@@ -1,8 +1,6 @@
 # Importing the necessary variables from the 'movies' module
 from movies import training_set, training_labels, validation_set, validation_labels
 
-## HELLO WORLD!
-
 def distance(movie1, movie2):
     # Initialize the squared difference
     squared_difference = 0
