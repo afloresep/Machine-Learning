370ef2e32aa57889fbda172609084fc09a47b00a
alex desktop <afloresep01@gmail.com>
2023-06-10 18:50:25 +0200

    Unsupervised Learning models

---
+++ Unsupervised_Learning/K-Means-Clustering.py
@@ -0,0 +1,34 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+samples = iris.data
+
+x = samples[:,0]
+y = samples[:,1]
+
+sepal_length_width = np.array(list(zip(x, y)))
+
+# Step 1: Place K random centroids
+
+k = 3
+
+centroids_x = np.random.uniform(min(x), max(x), size=k)
+centroids_y = np.random.uniform(min(y), max(y), size=k)
+
+centroids = np.array(list(zip(centroids_x, centroids_y)))
+
+# Step 2: Assign samples to nearest centroid
+
+# Distance formula
+
+# Cluster labels for each point (either 0, 1, or 2)
+
+# A function that assigns the nearest centroid to a sample
+
+# Assign the nearest centroid to each sample
+
+# Print labels
